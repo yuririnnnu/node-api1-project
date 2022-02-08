5822c66776183a6066e5bc6414a97eeee1b688d4
Yuriko Takamiya <yurikotakamiya@YurikonoMacBook-Pro-2.local>
2022-02-07 20:16:34 -0500

    done

--- api/server.js
+++ api/server.js
@@ -1,3 +1,90 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const userModel = require('./users/model')
+const server = express()
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json());
+
+server.get('/api/users', (req, res)=>{
+    userModel.find()
+    .then(users =>{
+        res.json(users)
+    })
+    .catch(() =>{
+        res.status(500).json({message:"could not get users"})
+    })
+})
+
+server.get('/api/users/:id', (req, res)=>{
+    const { id } = req.params
+    userModel.findById(id)
+    .then(user=>{
+        if(user == null){
+            res.status(404).json({message:"does not exist"})
+        } else {
+            res.json(user)
+        }
+    })
+    .catch(()=>{
+        res.status(404).json({message:"does not exist"})
+    })
+})
+
+server.post('/api/users', (req, res)=>{
+    const body = req.body;
+    if(!body.name || !body.bio){
+        res.status(400).json({message:"provide name and bio"})
+    } else {
+        userModel.insert(body)
+        .then(user => {
+            res.status(201).json(user)
+        })
+        .catch(() => {
+            res.status(400).json({message:"user could not be added"})
+        })
+    }
+})
+
+server.delete('/api/users/:id', (req, res)=> {
+    const { id } = req.params;
+    userModel.remove(id)
+    .then(user => {
+        if (user == null) {
+            res.status(404).json({message:"does not exist"})            
+        } else {
+            res.json(user)
+        }
+    })
+    .catch(() => {
+        res.status(404).json({message:"does not exist"})
+    })
+})
+
+server.put('/api/users/:id', async (req, res)=>{
+    const { id } = req.params;
+    
+    try{
+        let body = req.body;
+        if(!body.name){
+            res.status(400).json({message:"provide name and bio"})
+            return;
+        } else if (!body.bio){
+            res.status(400).json({message:"provide name and bio"})
+            return;            
+        } else {
+            let newUser = await userModel.update(id, body)
+            if(newUser == null){
+                res.status(404).json({message:"does not exist"})
+                return;
+            } else {
+                res.status(200).json(newUser)
+                return;
+            }
+        }
+    }catch(e){
+        res.status(404).json({message:"does not exist"})
+    }
+
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
+
+server.listen(port, () => {
+    console.log("server 9000 is connected")
+});
